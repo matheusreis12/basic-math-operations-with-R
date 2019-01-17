dac59fd44dd0de7e8397c260434fb5f0732366f6
Matheus Reis <45612748+matheusreis12@users.noreply.github.com>
2019-01-16 22:54:23 -0200

    Add files via upload

---
+++ Basic_Operations.R
@@ -0,0 +1,42 @@
+# Sum operation.
+
+# For the sum operation is used the sum operator, the "+" signal, or "plus" signal.
+ #For example, in a simple sum operation using two values:
+
+  2 + 3
+  
+# This is the result of the sum, the "5" value.
+# Now, we will enter with more values.
+  
+  9 + 3 + 5 + 8 + 9 + 9
+
+# The operation accept to enter with more values in the sentence of the question.
+
+
+# Subtraction Operation.
+  
+  
+# The subtraction operation is represented for the signal "-".
+# For example:
+  
+  3 - 5
+  
+  5 - 2
+  
+  
+# Division Operation
+  
+  3 / 5
+  
+# Attencion, don't use "," for to represent decimal number, use ".".
+
+  
+# Multiplication Operation
+  
+  3 * 5
+
+
+  
+  
+
+  
